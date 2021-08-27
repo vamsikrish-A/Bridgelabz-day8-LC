3bfbbc8c4d816cef0cd5f616101391b2a47e8ffa
vamsikrish-A <krishnavamsi165@gmail.com>
2021-08-27 14:17:14 +0530

    uc1 updated with methods

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -1,37 +1,20 @@
 package com.bridgelabz;
 
-import java.util.Scanner;
-
 public class LineComparison {
-    public static int lengthOfLine1() {
-        int x1,x2,y1,y2;
-        double ds;
-        Scanner sc=new Scanner(System.in);
-
-        System.out.println("enter x1 point");
-
-        x1=sc.nextInt();
-
-        System.out.println("enter y1 point");
-
-        y1=sc.nextInt();
-
-        System.out.println("enter x2point");
-
-        x2=sc.nextInt();
-
-        System.out.println("enter y2 point");
-
-        y2=sc.nextInt();
-        ds=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-        System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+ds);
-        int l1 = (int) ds;
-        return l1;
-    }
 
     public static void main(String arg[]) {
         System.out.println("Welcome to Line Comparison.");
-        lengthOfLine1();
+        int line1 = lengthOfLine(2,1,3,2);
+        
+    }
+
+    private static int lengthOfLine(int x1, int x2, int y1, int y2) {
+        double ds;
+        ds = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("distancebetween" + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ")===>" + ds);
+        int l1 = (int) ds;
+        return l1;
+
     }
 
 }
